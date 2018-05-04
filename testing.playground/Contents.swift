fc16191e463838258782939a0e181511969478db
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-05-04 14:45:35 -0400

    exploring dictionaries and dictionaries of arrays

--- testing.playground/Contents.swift
+++ testing.playground/Contents.swift
@@ -56,7 +56,7 @@ print(optional==nil) // should give us false bc this isnt nil, nil means value i
 var optional2: String? = "HELLO"
 var greeting = "hello"
 if let name = optional2{
-    greeting = "hello, \(name) "  // use the escape character to insert a variable into the string
+    greeting = "hello, \(name)"  // use the escape character to insert a variable into the string
 }
 
 print(greeting)
@@ -64,11 +64,48 @@ print(greeting)
 
 // can also define defaults in case one isnt present
 
-let carcolor: String? = nil
+let carcolor: String? = "issacolor"
 let cartype: String = "honda"
 
 let describecar = "that car is a \(carcolor ?? cartype)" // if there is no carcolor i.e. null, describe with car type
 print(describecar)
+
+
+// switches can use any type of data
+// you automatically break out of the switch statement if something is executed, so don't need to break
+let vegetable = "red pepper"
+switch vegetable {
+case "celery": // cases for strings! not just ints
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?") // make note of the escape character
+default:
+    print("Everything tastes good in soup.") // this is our default case, will get executed if nothing true
+}
+
+// let's experiement with iterating over a dictionary
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13], // dictionary where elements are arrays
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (key, value) in interestingNumbers { // iterate through the keys, then iterate through the values
+    for number in value { // go through every value, and if it's the largest so far set as largest
+        if number > largest {
+            largest = number
+        }
+    }
+}// ** note ^^^ the names of the key, value are arbitrary and can be anything!!!! ***
+print(largest)
+
+
+
+
+
+
 
 
 
