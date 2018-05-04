3a5161bdf8d740e8731496b7b29f4626b29343b3
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-05-04 16:32:31 -0400

    exploring objects and classes in swift

--- testing.playground/Contents.swift
+++ testing.playground/Contents.swift
@@ -124,13 +124,110 @@ for i in 0..<4 { // i goes from 0 to less than four i.e. 0, 1, 2, 3
 }
 
 
-// FUNCTIONS IN SWIFT
+// FUNCTIONS IN SWIFT -- use keyword func, then put params, then put -> return type, then put {} body and return smthng
 
 // argument labels, the things in the func definition = params, actually passed = args
 func greet(_ person: String, on day: String) -> String { // used underscore which means we don't have to specify that it's a person
     return "Hello \(person), today is \(day)."
 }
 print(greet("John", on: "Wednesday")) // normally would put key: person instead
+
+
+
+// FUNCTIONS returning more than one value, make it a tuple as below
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) { // return min, max and sum
+    var min = scores[0]             // notice how integer type is Int capitalized
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.0) // dot notation accesses the 0th element of the tuple and displays that
+
+
+// FUNCTIONS are first class types i.e. they can return other functions
+func makeIncrementer() -> ((Int) -> Int) { // returns a function that takes an int and returns an int
+    func addOne(number: Int) -> Int { // takes an integer and returns an integer
+        return 1 + number
+    }
+    return addOne // makeIncrementer returns a function add one
+}
+var increment = makeIncrementer() // incremenet now has the value of the function addOne
+print(increment(7)) // now when we pass 7 to it it gets 1 + 7 so it returns 8
+
+
+
+// FUNCTIONS can take other functions as arguments, specify in param, condition is a function that takes an int and returns boolean
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool { // this whole thing returns a boolean
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 7, 12]
+print(hasAnyMatches(list: numbers, condition: lessThanTen)) // should be true because we have a 7 in the numbers, and our condition that we're
+                                                            // checking to see if matched is if they're less than 10
+
+
+// CLOSURES are the general case of functions, i.e. can be called later
+numbers.map({ (number: Int) -> Int in // in separates param and return type from rest of closure
+    let result = 3 * number // return a result that is 3*the number passed in
+    return result
+} /* this stuff in the braces was a closure*/ )
+
+
+
+// OBJECTS AND CLASSES
+
+class Shape { // MAKE A CLASS called shape, peep the caps
+    var numberOfSides = 0
+    func simpleDescription() -> String { // has a function , has a field num of sides
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+var shape = Shape() // create an instance by using parenthesis
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescription() // then use dot notation to access the functions or params
+
+// new class named shape
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name // init is the initializer which allows the shape to be created with values
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    } // NOTE: every property must be assigned, either in the init or initialized in the body as numberOfSides is w = 0
+} // can also have deinit to cleanup before destroying/deallocating
+
+
+
+
+
+
+
+
+
 
 
 
