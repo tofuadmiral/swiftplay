f36ea0c4cec67829d7ff1bd430be51bbe0eac762
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-05-04 13:34:18 -0400

    testing typecasting variables

--- testing.playground/Contents.swift
+++ testing.playground/Contents.swift
@@ -4,6 +4,13 @@ import UIKit
 
 var str = "Hello, playground"
 
-print("hello world")
+print(str)
 
+var myVariable = 42
+
+let myConstant = 50
+
+var myexplicit: Double = 70.00
+
+print(myexplicit)
 
