891972dbf9932c72c5c2fe49002fd3c2a145336e
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-05-04 13:54:47 -0400

    playing with dictionaries

--- testing.playground/Contents.swift
+++ testing.playground/Contents.swift
@@ -14,3 +14,22 @@ var myexplicit: Double = 70.00
 
 print(myexplicit)
 
+let label = "this is how you typecast"
+let number = 30
+
+print(label + " " +  String(number))
+
+
+// triple quotes means multi line string
+let mystring = """
+hello this is a
+really long string that spans
+ more than one line
+"""
+
+// initializer syntax for arrays and dictionaries
+let emptyArray = [String]() // use to represent an array of strings
+let emptyDic = [String: Float])() // use to represent names and marks
+
+
+
