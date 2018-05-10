b6e5b2f10fef84a43e49ff030921019b47a6ffc6
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-05-10 19:11:29 -0400

    exploring outlets for my UI elements

--- Food Tracker/Food Tracker/ViewController.swift
+++ Food Tracker/Food Tracker/ViewController.swift
@@ -9,8 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     // inherits from UIViewController, then overrides stuff depending on what you want from your app
+    
+    // the comment below is a special one that indicates that the next section contains properties
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    /*
+        the above statement implies that UITextField is an unwrapped optional
+        use the outlet to either modify a value or to access the value inside something
+        outlets let you call stuff, but you also need to respond to user interaction. i.e. ACTIONS
+     */
     
     override func viewDidLoad() {
         super.viewDidLoad()
