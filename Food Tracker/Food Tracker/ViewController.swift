f4c911555572eacaecebb896cfc19deb846e6088
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-05-20 18:46:52 -0400

    modifying the delegate of the uitextfield

--- Food Tracker/Food Tracker/ViewController.swift
+++ Food Tracker/Food Tracker/ViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    //MARK: Properties
+class ViewController: UIViewController, UITextFieldDelegate { // adopting the uitextfield delegate so we can act on behalf of the uitextfield and get information from there
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel! // only need outlets if we plan to modify or access these values, AND use actions to actually modify or act on these outlets.
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // handle user's input from the text field using a callback
+        nameTextField.delegate = self // self refers to viewcontroller class
+        // we want to set the delegate of the textfield as the viewcontroller so it can handle any input necessary
+        // we need to implement two of the methods used by the UITextField Delegate, not all 8. these eight will be necessary for the processing of the input from the user in order to change the text of the label
     }
 
     override func didReceiveMemoryWarning() {
