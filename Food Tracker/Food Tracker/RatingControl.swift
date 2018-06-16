bd0c6d1e7f7256bcd200efc7eecec360c172eb46
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-06-16 18:04:28 -0400

    set up button in custom view

--- Food Tracker/Food Tracker/RatingControl.swift
+++ Food Tracker/Food Tracker/RatingControl.swift
@@ -12,12 +12,27 @@ class RatingControl: UIStackView {
     //MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUpButtons()
     } // must use required to inherit the initaliazers that we HAVE to have, otherwise will take none if we specifiy our own
     required init(coder: NSCoder){
-        super.init(coder: <#T##NSCoder#>)
+        super.init(coder: coder)
+        setUpButtons()
     }
     
     //MARK: Private Methods
+    private func setUpButtons(){
+        // create a button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // constraints for the button, first disable inherited ones then define our own for height and width
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // add the button to the stack, managed by ratingcontrol stack view, which will create our constraints
+        addArrangedSubview(button)
+    }
     
     
     
