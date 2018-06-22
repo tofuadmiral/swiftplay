7f0de33e43bde73f199e7d9f45ac04936412cb22
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-06-22 10:33:36 -0400

    drew buttons within custom view

--- Food Tracker/Food Tracker/RatingControl.swift
+++ Food Tracker/Food Tracker/RatingControl.swift
@@ -10,15 +10,37 @@ import UIKit
 
 class RatingControl: UIStackView {
     //MARK: Initialization
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUpButtons()
     }
     
     required init(coder: NSCoder){
         super.init(coder: coder)
+        setUpButtons()
     }
     
     // initializers different in swift, if you call one you don't inherit ANY anymore, but if you don't call any you inherit all of them
+    
+    //MARK: Private Methods
+    
+    private func setUpButtons(){
+        // create the buttons
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // add some constraints to the button, so resizes properly
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // add button to the stack
+        addArrangedSubview(button)
+        
+    }
+    
+    
     
     
     // this will be important later but we don't need it right now
